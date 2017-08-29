006d6c9953e67075db2a27946caaccb8bfd7b932
ElectrifyPro <ElectrifyPro@users.noreply.github.com>
2017-08-28 18:59:02 -0700

    Update everNote_user.java

--- evernote/everNote_user.java
+++ evernote/everNote_user.java
@@ -1,6 +1,6 @@
 public class everNote_user {
 	public static void main (String args []) throws Exception {
-		everNote.token_g = "";
+		everNote.token_g = ""; //paste API Key here
 		everNote.main (args);
 	}
 }
